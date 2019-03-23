1155969f6e6803748e11862425e83cf9d958f5f3
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2019-03-23 17:27:54 +0200

    errors handling and input validation

--- app/src/Controller/MoviesController.php
+++ app/src/Controller/MoviesController.php
@@ -7,6 +7,7 @@ use FOS\RestBundle\Controller\ControllerTrait;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\Validator\ConstraintViolationListInterface;
 
 class MoviesController extends AbstractController
 {
@@ -27,8 +28,12 @@ class MoviesController extends AbstractController
      * @ParamConverter("movie", converter="fos_rest.request_body")
      * @Rest\NoRoute()
      */
-    public function postMoviesAction(Movie $movie)
+    public function postMoviesAction(Movie $movie, ConstraintViolationListInterface $validationErrors)
     {
+        if (count($validationErrors) > 0) {
+            throw new \HttpException(400, "Invalid or incomplete data");
+        }
+
         $em = $this->getDoctrine()->getManager();
         $em->persist($movie);
         $em->flush();
@@ -36,10 +41,11 @@ class MoviesController extends AbstractController
         return $movie;
     }
 
-    public function deleteMovieAction($movieId)
+    /**
+     * @Rest\View()
+     */
+    public function deleteMovieAction(?Movie $movie)
     {
-        $movie = $this->getDoctrine()->getRepository(Movie::class)->find($movieId);
-
         if (null === $movie) {
             return $this->view(null,404);
         }
@@ -47,5 +53,17 @@ class MoviesController extends AbstractController
         $em = $this->getDoctrine()->getManager();
         $em->remove($movie);
         $em->flush();
+    }
+
+    /**
+     * @Rest\View()
+     */
+    public function getMovieAction(?Movie $movie)
+    {
+        if (null === $movie) {
+            return $this->view(null,404);
+        }
+
+        return $movie;
     }
 }
